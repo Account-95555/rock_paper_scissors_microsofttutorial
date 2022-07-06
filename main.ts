ae33448b85e84ff61e4de644f12d5ee4738126f2
Account-95555 <100207649+Account-95555@users.noreply.github.com>
2022-07-06 08:10:35 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+let outcome = 0
+input.onGesture(Gesture.Shake, function () {
+    outcome = randint(1, 3)
+    if (outcome == 1) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    } else if (outcome == 2) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
+})
